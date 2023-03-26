ef9e8521f1b3d33722eea80f82233c0f9975574b
Szczurowsky <kamilszczurowski@hotmail.com>
2023-03-26 17:32:59 +0200

    fix: change versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "pl.szczurowsky.velocityloadbalancer"
-version = "1.0." + "git rev-list --count HEAD".runCommand(workingDir = rootDir)
+version = "1.0"
 
 repositories {
 
@@ -20,18 +20,3 @@ dependencies {
     compileOnly("com.velocitypowered:velocity-api:3.1.1")
     annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
 }
-
-fun String.runCommand(
-    workingDir: File = File("."),
-    timeoutAmount: Long = 60,
-    timeoutUnit: TimeUnit = TimeUnit.SECONDS
-): String = ProcessBuilder(split("\\s(?=(?:[^'\"`]*(['\"`])[^'\"`]*\\1)*[^'\"`]*$)".toRegex()))
-    .directory(workingDir)
-    .redirectOutput(ProcessBuilder.Redirect.PIPE)
-    .redirectError(ProcessBuilder.Redirect.PIPE)
-    .start()
-    .apply { waitFor(timeoutAmount, timeoutUnit) }
-    .run {
-        val error = errorStream.bufferedReader().readText().trim()
-        inputStream.bufferedReader().readText().trim()
-    }
